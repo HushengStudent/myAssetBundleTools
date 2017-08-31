524be8b1d28ad4074cf216936dc1a67be4d79c4d
胡胜 <1258164851@qq.com>
2017-08-31 19:17:28 +0800

    test script

---
+++ Assets/_TestFolder/Editor/BuildABs.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System;
+
+public static class BuildABs
+{
+    private static string outPutPath = "Assets/_TestFolder/AssetBundles"; 
+
+    [MenuItem("ABTest/Build")]
+    public static void BuildTest()
+    {
+        BuildPipeline.BuildAssetBundles(outPutPath);
+        AssetDatabase.Refresh();
+    }
+
+    [MenuItem("ABTest/Manifest")]
+    public static void ManifestTest()
+    {
+        AssetBundle ab = AssetBundle.LoadFromFile(outPutPath + "/AssetBundles");
+        if (ab == null) return;
+
+        AssetBundleManifest abManifest = ab.LoadAsset("AssetBundleManifest") as AssetBundleManifest;
+
+        string[] abs = abManifest.GetAllAssetBundles();
+        string[] abAllDepend;
+        string[] abDirectDepend;
+        foreach (string temp in abs)
+        {
+            Debug.LogError("--->assetbundle:" + temp);
+            abAllDepend = abManifest.GetAllDependencies(temp);
+            foreach (string allStr in abAllDepend)
+            {
+                Debug.LogError("--->GetAllDependencies"+"--->"+temp+":" + allStr);
+            }
+            abDirectDepend = abManifest.GetDirectDependencies(temp);
+            foreach (string directStr in abAllDepend)
+            {
+                Debug.LogError("--->GetDirectDependencies" + "--->" + temp + ":" + directStr);
+            }
+        }
+    }
+}

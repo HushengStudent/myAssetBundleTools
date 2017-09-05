e144ffbdc7c3c34c0bb80a121f33db2d6970272b
胡胜 <husheng.student@foxmail.com>
2017-09-05 21:18:52 +0800

    some code commit

--- Assets/Scripts/Manager/AssetBundleMgr.cs
+++ Assets/Scripts/Manager/AssetBundleMgr.cs
@@ -12,8 +12,35 @@
 
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class AssetBundleMgr : SingletonManager<AssetBundleMgr>
 {
+    /// <summary>
+    /// 加载出来的AssetBundle缓存;
+    /// </summary>
+    private Dictionary<string, AssetBundle> assetBundleCache = new Dictionary<string, AssetBundle>();
 
+    /// <summary>
+    /// 加载出来的AssetBundle引用计数;
+    /// </summary>
+    private Dictionary<string, int> assetBundleReference = new Dictionary<string, int>();
+
+    public void AssetBundleMainManifestInit()
+    {
+        AssetBundle assetBundle = AssetBundle.LoadFromFile(FilePathUtil.assetBundlePath+"AssetBundle");
+        if (assetBundle != null)
+        {
+
+        }
+        else
+        {
+            Debug.LogError(string.Format("[AssetBundleMgr]Load AssetBundle {0} failure", FilePathUtil.assetBundlePath+"AssetBundle"));
+        }
+    }
+
+    //public AssetBundle LoadAssetBundleSyn(string path)
+    //{
+
+    //}
 }
